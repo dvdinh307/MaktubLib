c941e1595ab2fbda55b1c0297fd235952011e01c
dinhdv <dinhdv@hanelsoft.vn>
2017-06-19 10:34:17 +0700

    dinhdv push first class.

---
+++ app/src/main/java/maktub/vn/maktublib/utils/BaseActivity.java
@@ -0,0 +1,24 @@
+package maktub.vn.maktublib.utils;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public abstract class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        // TODO Auto-generated method stub
+        super.onCreate(arg0);
+        setContentView(getLayoutId());
+        initVariable();
+        initControls();
+    }
+
+    protected abstract int getLayoutId();
+
+    protected abstract void initVariable();
+
+    protected abstract void initControls();
+
+
+}
