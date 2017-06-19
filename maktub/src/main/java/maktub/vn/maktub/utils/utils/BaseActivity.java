a7e314d9b7445ec386ca8535ba57812910f72cef
dinhdv <dinhdv@hanelsoft.vn>
2017-06-19 16:36:18 +0700

    dinhdv change abstract initcontrols

--- maktub/src/main/java/maktub/vn/maktub/utils/utils/BaseActivity.java
+++ maktub/src/main/java/maktub/vn/maktub/utils/utils/BaseActivity.java
@@ -1,24 +1,24 @@
 package maktub.vn.maktub.utils.utils;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 public abstract class BaseActivity extends FragmentActivity {
-
     @Override
-    protected void onCreate(Bundle arg0) {
-        // TODO Auto-generated method stub
-        super.onCreate(arg0);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         initVariable();
-        initControls();
+        initControls(savedInstanceState);
     }
+
 
     protected abstract int getLayoutId();
 
     protected abstract void initVariable();
 
-    protected abstract void initControls();
+    protected abstract void initControls(Bundle bundle);
 
 
 }
